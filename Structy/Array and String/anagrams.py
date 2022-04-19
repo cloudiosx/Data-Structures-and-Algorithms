25a892a5a4133bd8bab53060a2328fb265c5a101
John Kim <cloudiosx@gmail.com>
2022-04-19 10:22:33 -0400

    Solve anagrams.py

---
+++ Structy/Array and String/anagrams.py
@@ -0,0 +1,20 @@
+# Time complexity: O(n + m) | Space complexity: ~O(n + m)
+def anagrams(s1, s2):
+	return char_count(s1) == char_count(s2)
+
+def char_count(s):
+  count = {}
+  
+  for char in s:
+    if char not in count:
+      count[char] = 0
+    
+    count[char] += 1
+  
+  return count
+                      
+# Importing Counter structure from Collections built-in module
+from collections import Counter
+
+def anagrams(s1, s2):
+  return Counter(s1) == Counter(s2)
