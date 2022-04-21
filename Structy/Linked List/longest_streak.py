89e58a4011b2f678f6218933d298e3660026f4b0
John Kim <cloudiosx@gmail.com>
2022-04-21 13:30:49 -0400

    Add solution for longest_streak.py

--- Structy/Linked List/longest_streak.py
+++ Structy/Linked List/longest_streak.py
@@ -9,21 +9,18 @@ def longest_streak(head):
   max_streak = 0
   current_streak = 0
   prev_val = None
-  current = head
-
-  while current is not None:
-    if prev_val is None:
+  
+  current_node = head
+  while current_node is not None:
+    if current_node.val == prev_val:
       current_streak += 1
-
-    if prev_val is not None and prev_val == current.val:
-      current_streak += 1
-    elif prev_val != current.val:
+    else:
       current_streak = 1
-
+  
+    prev_val = current_node.val
     if current_streak > max_streak:
       max_streak = current_streak
-
-    prev_val = current.val
-    current = current.next
-
+    
+    current_node = current_node.next
+    
   return max_streak
