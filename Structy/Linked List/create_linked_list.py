22dc5f42a6fe4dd10e01808452c9d586c6c032a1
John Kim <cloudiosx@gmail.com>
2022-04-22 10:37:43 -0400

    Add solution for create_linked_lists.py

--- Structy/Linked List/create_linked_list.py
+++ Structy/Linked List/create_linked_list.py
@@ -3,17 +3,30 @@ class Node:
     self.val = val
     self.next = None
 
-# Time complexity: O(n) | Space complexity: O(n) | n = array length
+# Time complexity: O(n) | Space complexity: O(n) | n = length of values
 # Iterative
 def create_linked_list(values):
-  dummy_node = Node('')
-  tail = dummy_node
-  
-  for value in values:
-    tail.next = Node(value)
+  dummy_head = Node(None)
+  tail = dummy_head
+  for val in values:
+    tail.next = Node(val)
     tail = tail.next
+  return dummy_head.next
 
-  return dummy_node.next
+# Time complexity: O(n^2) | Space complexity: O(n) | n = length of values
+# Recursive
+def create_linked_list(values):
+  if len(values) == 0:
+    return None
+  head = Node(values[0])
+  head.next = create_linked_list(values[1:]) # slice creates new copy in Python
+  return head
 
-
-
+# Time complexity: O(n) | Space complexity: O(n) | n = length of values
+# Recursive
+def create_linked_list(values, i = 0):
+  if i == len(values):
+    return None
+  head = Node(values[i])
+  head.next = create_linked_list(values, i + 1)
+  return head
