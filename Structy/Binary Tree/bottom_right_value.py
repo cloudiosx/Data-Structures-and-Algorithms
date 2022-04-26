a16bbce50365d57d398c41421519763cfee9b3c9
John Kim <cloudiosx@gmail.com>
2022-04-26 19:08:24 -0400

    Add solution to bottom_right_value.py

---
+++ Structy/Binary Tree/bottom_right_value.py
@@ -0,0 +1,22 @@
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.left = None
+    self.right = None
+
+# BFT - Iterative
+from collections import deque
+def bottom_right_value(root):
+  queue = deque([root])
+
+  current = None
+  while queue:
+    current = queue.popleft()
+
+    if current.left:
+      queue.append(current.left)
+
+    if current.right:
+      queue.append(current.right)
+
+  return current.val
