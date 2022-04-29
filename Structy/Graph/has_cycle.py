f68cb3e95473d0b6cdd7dc52f785bf1cc0d8837d
John Kim <cloudiosx@gmail.com>
2022-04-29 16:50:18 -0400

    Complete has_cycle.py

---
+++ Structy/Graph/has_cycle.py
@@ -0,0 +1,25 @@
+# Time: O(n^2) | Space: O(n) | n = number of nodes
+
+def has_cycle(graph):
+  visited = set()
+  for start_node in graph:
+    if cycle_detect(graph, start_node, set(), visited):
+      return True
+  return False
+
+def cycle_detect(graph, node, visiting, visited):
+  if node in visited:
+    return False
+
+  if node in visiting:
+    return True
+
+  visiting.add(node)
+
+  for neighbor in graph[node]:
+    if cycle_detect(graph, neighbor, visiting, visited):
+      return True
+
+  visiting.remove(node)
+  visited.add(node)
+  return False
