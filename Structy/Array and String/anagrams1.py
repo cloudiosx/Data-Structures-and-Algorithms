60668f558ede5417929bc7bf6086deb2f0bb3b94
John Kim <cloudiosx@gmail.com>
2022-05-07 21:32:13 -0400

    Complete anagrams1.py

---
+++ Structy/Array and String/anagrams1.py
@@ -0,0 +1,20 @@
+def anagrams(s1, s2):
+	hashmap_s1 = {}
+	hashmap_s2 = {}
+
+	for char in s1:
+		if char not in hashmap_s1:
+			hashmap_s1[char] = 0
+
+		hashmap_s1[char] += 1
+
+	for char in s2:
+		if char not in hashmap_s2:
+			hashmap_s2[char] = 0
+
+		hashmap_s2[char] += 1
+
+	if hashmap_s1 == hashmap_s2:
+		return True
+
+	return False
